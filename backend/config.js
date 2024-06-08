7efb4702cc4ddd0902e800881adba7e858d4614d
shankarjatin <shankarjatin1005@gmail.com>
2024-06-08 10:20:27 +0530

    config file added for test

---
+++ backend/config.js
@@ -0,0 +1,10 @@
+// config.js
+module.exports = {
+    BASE_API_URL: 'http://20.244.56.144/test',
+    COMPANIES: ['AMZ', 'FLP', 'SNP', 'MYN', 'AZO'],
+    CATEGORIES: [
+        'Phone', 'Computer', 'TV', 'Earphone', 'Tablet', 'Charger', 'Mouse', 
+        'Keypad', 'Bluetooth', 'Pendrive', 'Remote', 'Speaker', 'Headset', 
+        'Laptop', 'PC'
+    ]
+};
